b6c3cda9cbf985faf309dfb9eb630f3fa16544ee
Jim <jpisano@cisco.com>
2018-06-03 12:19:26 -0400

    Added a local sql connector

--- application/views.py
+++ application/views.py
@@ -11,25 +11,37 @@ def index():
         print('Got a GET: ', request.get_data())
 
     coverages = Coverage.newest_name(12)
-    print(type(coverages))
     return render_template('index.html', coverages=coverages)
+
+
+@application.route('/list', methods=['GET', 'POST'])
+def list():
+    if request.method == 'POST':
+        print('Got a POST: ', request.get_data())
+    elif request.method == 'GET':
+        print('Got a GET: ', request.get_data())
+
+    coverages = Coverage.newest_name(12)
+    return render_template('list.html', coverages=coverages)
 
 
 @application.route('/input', methods=['GET', 'POST'])
 def input():
-    coverages = [1,2,3,4]
-    return render_template('input.html', coverages=coverages)
+    if request.method == 'POST':
+        print('Got a POST: ', request.get_data())
+    elif request.method == 'GET':
+        print('Got a GET: ', request.get_data())
+
+    return render_template('input.html')
 
 
 @application.errorhandler(404)
 def page_not_found(e):
-    print('404 error')
     return render_template('404.html'),404
 
 
 @application.errorhandler(500)
 def server_error(e):
-    print('500 error')
     return render_template('500.html'),500
 
 
